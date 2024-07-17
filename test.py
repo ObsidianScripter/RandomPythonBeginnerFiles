ce655e5ff1fee7d9a3062110501e34975951d74f
ObsidianScripter <141820441+ObsidianScripter@users.noreply.github.com>
2024-07-17 22:29:42 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+name = " "
+
+while name != "Christian":
+	#as long as name inputed isn't 'Christian' then the pragram will keep
+#asking you to insert the correct name
+	print("Please insert the correct name")
+	name = input() # this is where you insert the name
+
+print("Thank you!")
